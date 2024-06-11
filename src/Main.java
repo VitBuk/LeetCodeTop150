cc3420fd29aceafc7cf49fef9b5ed74cd2bdb1ef
VitBuk <vitbuk@gmail.com>
2024-06-11 17:16:18 +0300

    Problem80 solved

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import Problem26.Problem26;
 import Problem27.Problem27;
+import Problem80.Problem80;
 import Problem88.Problem88;
 
 public class Main {
     public static void main(String[] args) {
         int[] nums = {0,0,0,0,1,2,2,2,2,2,2,3,3,3,4};
-        int k = Problem26.removeDuplicates(nums);
+        int k = Problem80.removeDuplicates(nums);
         System.out.println();
         System.out.print("k = " + k);
     }
