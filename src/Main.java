b4ff0f965b885811cbe5003d25ddb27fca38fb66
VitBuk <vitbuk@gmail.com>
2024-06-11 16:51:44 +0300

    Problem 26 solved

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
+import Problem26.Problem26;
 import Problem27.Problem27;
 import Problem88.Problem88;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {3,2,3,2,3,3,2,0,1,2,2,3,0,4,2};
-        int val = 2;
-        int k = Problem27.removeElement(nums, val);
+        int[] nums = {0,0,0,0,1,2,2,2,2,2,2,3,3,3,4};
+        int k = Problem26.removeDuplicates(nums);
         System.out.println();
         System.out.print("k = " + k);
     }
