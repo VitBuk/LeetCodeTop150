266d8b78eba3f2c696b06c7274ec1ef69358100c
VitBuk <vitbuk@gmail.com>
2024-06-11 15:32:22 +0300

    Problem 27 shorter/better solution

--- src/Problem27/Problem27.java
+++ src/Problem27/Problem27.java
@@ -3,24 +3,14 @@ package Problem27;
 public class Problem27 {
 
     public static int removeElement(int[] nums, int val){
-        int temp;
         int k = 0;
 
         for (int i=0; i<nums.length; i++) {
-            if (nums[i] == val) {
-                for (int j=nums.length-1; j>0; j--) {
-                    if (i > j ) break;
-                    if (nums[j] != val) {
-                        temp = nums[j];
-                        nums[j] = nums[i];
-                        nums[i] = temp;
-                        k++;
-                        break;
-                    }
-                }
-            } else k++;
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
         }
-
         return k;
     }
 }
