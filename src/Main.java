2f409e9a06b217d48012ac5b8b77a090df2a7857
VitBuk <vitbuk@gmail.com>
2024-07-04 17:22:26 +0300

    Problem28 solve. Beets 100%

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
 import Problem125.Problem125;
-import Problem13.Problem13;
-import Problem14.Problem14;
-import Problem169.Problem169;
-import Problem189.Problem189;
-import Problem26.Problem26;
-import Problem27.Problem27;
-import Problem58.Problem58;
-import Problem80.Problem80;
-import Problem88.Problem88;
+import Problem28.Problem28;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "0P";
-        System.out.println(Problem125.isPalindrome(s));
+        String haystack = "a!saddbutsad";
+        String needle = "sad";
+        System.out.println(Problem28.strStr(haystack, needle));
     }
 }
