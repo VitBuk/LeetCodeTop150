a080cf5e4893162fb578ed8632a70bd5fdf56e34
VitBuk <vitbuk@gmail.com>
2024-07-09 15:59:30 +0300

    start f problem383

---
+++ src/Problem383/Problem383.java
@@ -0,0 +1,11 @@
+package Problem383;
+
+public class Problem383
+    /*Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.*/
+
+        public boolean canConstruct(String ransomNote, String magazine) {
+            
+        }
+}
