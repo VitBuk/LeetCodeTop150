14d3670e3b5444b2d23e2e277e6cd1fdef9b85bf
student13 <student13@ideaportriga.com>
2024-09-18 19:21:31 +0300

    Refactoring: cleaning

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
-import Problem383.Problem383;
 public class Main {
     public static void main(String[] args) {
-        String ransomNote = "aa";
-        String magazine = "ab";
-        System.out.println(Problem383.canConstruct(ransomNote, magazine));
+
     }
 }
